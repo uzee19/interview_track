cb3ec69651a2fdb645b045026addae0c60ac20ae
uranjan445 <58565275+uranjan445@users.noreply.github.com>
2021-02-04 13:38:48 +0530

    4feb

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     MongoURI : 'mongodb+srv://uzee20:mongodb@2020@cluster0.2to8l.mongodb.net/test?retryWrites=true&w=majority',
     ADMIN_EMAIL: 'rhit@admin.com' || 'rohit@admin.com',
-    ADMIN_PASSWORD: 'testjs' || 'passjs'
+    ADMIN_PASSWORD: 'testsj' || 'passjs'
 
 
 }
