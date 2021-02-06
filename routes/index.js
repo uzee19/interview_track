fd357ecfcb4fc507d328693e76913970aab0f1a6
uranjan445 <58565275+uranjan445@users.noreply.github.com>
2021-02-06 12:25:49 +0530

    eye icon

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,7 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
-// Dashboard
-router.get('/topics', ensureAuthenticated, (req, res) =>
-  res.render('topics', {
-    user: req.user
-  })
-);
+
 
 
 
